b127dc24a573785d557aca7314672b7b107c17fb
fronz <z233@msn.com>
2021-07-26 22:46:21 +0800

    build: update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
