dbe33ddfb80b71400848fb7892067a85b4ec6f40
fronz <z233@msn.com>
2021-07-28 13:13:12 +0800

    package: remove useless stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,8 +30,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/fullpage', mode: 'client' },
-    { src: '~/plugins/lottie' }
+    { src: '~/plugins/fullpage', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- plugins/lottie.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue"; // import lottie-vuejs
- 
-Vue.use(LottieAnimation); // add lottie-animation to your global scope
