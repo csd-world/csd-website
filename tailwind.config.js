4f873adb081af93f2d555c6f1dfbadd5662f06f0
fronz <z233@msn.com>
2021-07-28 15:47:58 +0800

    fix: nav links gone

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,38 +8,37 @@ module.exports = {
   plugins: [
     textShadow
   ],
-  important: true,
-    theme: {
-      extend: {
-        transitionProperty: {
-          'height': 'height'
+  theme: {
+    extend: {
+      transitionProperty: {
+        'height': 'height'
+      },
+      borderRadius: {
+        '1/2': '50%',
+        '1/4': '25%'
+      },
+      padding: {
+        '1/2': '50%',
+        '1/4': '25%',
+        '1/6': '16.667%',
+        '1/8': '12.5%',
+        '1/10': '10%',
+        '1/12': '8.333%',
+        'full': '100%',
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#5222d0',
+          darker: darken('#5222d0', 0.05)
         },
-        borderRadius: {
-          '1/2': '50%',
-          '1/4': '25%'
-        },
-        padding: {
-          '1/2': '50%',
-          '1/4': '25%',
-          '1/6': '16.667%',
-          '1/8': '12.5%',
-          '1/10': '10%',
-          '1/12': '8.333%',
-          'full': '100%',
-        },
-        colors: {
-          primary: {
-            DEFAULT: '#5222d0',
-            darker: darken('#5222d0', 0.05)
-          },
-          info: {
-            DEFAULT: 'rgba(0, 0, 0, 0.2)',
-            darker: 'rgba(0, 0, 0, 0.25)'
-          }
-        },
-        minWidth: {
-          '32': '8rem'
+        info: {
+          DEFAULT: 'rgba(0, 0, 0, 0.2)',
+          darker: 'rgba(0, 0, 0, 0.25)'
         }
+      },
+      minWidth: {
+        '32': '8rem'
       }
     }
+  }
 }
