e015a8588acf4e58df642b43b8ffa38c32512c1b
fronz <z233@msn.com>
2021-09-17 18:18:33 +0800

    refactor: apply require reason

--- pages/apply/index.vue
+++ pages/apply/index.vue
@@ -51,7 +51,10 @@
           </div>
           <BaseCheckbox v-model="checked" :label="'我有编程基础'" />
           <BaseTextarea v-show="checked" :name="'prgExp'" :label="'聊聊你学过的东西，以及用来做过哪些有趣的事'" />
-          <BaseTextarea :name="'applyReason'" :label="'说说你为什么想加入软件部'" />
+          <BaseTextarea 
+            :rules="'required'"
+            :name="'applyReason'" 
+            :label="'说说你为什么想加入软件部'" />
           <button 
             :class="{ 'loading': loading }"
             :disabled="loading"  
